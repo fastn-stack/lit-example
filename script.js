9064da404296bcecc1150332f91f37fcfb802911
Arpita-Jaiswal <Arpita-Jaiswal@users.noreply.github.com>
2023-03-13 08:53:38 +0000

    deploy: 8b5cf564edbec7f42f8eca7ea2ba6d8b8ba34d5a

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
-import {LitElement, html} from 'https://cdn.skypack.dev/lit';
+import {LitElement, html, css} from 'https://cdn.skypack.dev/lit';
 class LearnLit extends LitElement {
     static properties = {
-        count: {type: Number}
+        count: {type: Number},
+        data: {type: Object}
     }
 
     constructor() {
         super();
         this.count = 0;
+        this.data = window.ftd.component_data(this);
     }
 
     render() {
         return html`
+            <div class="card">
             <div>The count is: ${this.count}</div>
-            <button @click=${() => (this.count++)}>Click me</button>
+            <div class="title">${this.data.name.get()}</div>
+            <button @click=${this._onClick}>Click me</button>
+            </div>
         `;
     }
+
+    _onClick() {
+        this.count++;
+        this.data.name.set(this.data.name.get() + ":" +this.count)
+    }
+
+    static styles = css`
+    .card {
+      padding: 20px;
+      background-color: yellow;
+      margin: 20px;
+      gap: 10px;
+      display: flex;
+      flex-direction: column;
+      border-color: orange;
+      border-width: 13px;
+      border-style: dotted;
+    }
+    
+    .title {
+      font-size: 18px;
+      color: #0000FF;
+      font-weight: bold;
+      font-style: italic;
+    }
+    `;
 }
 customElements.define('learn-lit', LearnLit);
